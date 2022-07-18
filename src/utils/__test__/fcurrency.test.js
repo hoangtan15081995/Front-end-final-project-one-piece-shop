db2633a3ca2540ba8ddd957b4f9abe97a78074b0
hoangtan15081995 <98004746+hoangtan15081995@users.noreply.github.com>
2022-07-18 22:05:39 +0700

    Update fcurrency.js

--- src/utils/__test__/fcurrency.test.js
+++ src/utils/__test__/fcurrency.test.js
@@ -5,6 +5,6 @@ test("test fcurrency number", () => {
   expect(fCurrency(12)).toEqual("$12");
 });
 
-test("test fcurrency string", () => {
-  expect(fCurrency("12.50")).toEqual("$12.50");
+test("test fcurrency float", () => {
+  expect(fCurrency(12.50)).toEqual("$12.50");
 });
--- src/utils/fcurrency.js
+++ src/utils/fcurrency.js
@@ -1,5 +1,8 @@
-import numeral from "numeral";
-
-export function fCurrency(number) {
-  return numeral(number).format(Number.isInteger(number) ? "$0,0" : "$0,0.00");
+"use strict";
+exports.__esModule = true;
+exports.fCurrency = void 0;
+var numeral_1 = require("numeral");
+function fCurrency(number) {
+    return (0, numeral_1["default"])(number).format(Number.isInteger(number) ? "$0,0" : "$0,0.00");
 }
+exports.fCurrency = fCurrency;
